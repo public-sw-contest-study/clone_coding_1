dc404ab8a4d47cc670fdeb4f9f8fa5cd911a996a
kosw <swko4512@gmail.com>
2023-06-30 12:26:24 +0900

    test

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert("hi");
+const a = 5;
+
+console.log(a + 2);
+console.log(a * 2);
+console.log(a / 2);
+
+const b = [1,2,3,4,5];
+console.log(b);
+console.log(b[2]);
+b.push("101");
+console.log(b);
+
+const c = {
+    name: "kosw",
+    points: 10,
+    fat: true,
+};
+
+console.log(c);
+console.log(c.name);
