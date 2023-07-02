d82f2c638e4b19885abdbe85054f8ad0b9f2e7a1
kosw <swko4512@gmail.com>
2023-07-03 02:37:48 +0900

    test

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
-const a = 5;
+// const a = 5;
 
-console.log(a + 2);
-console.log(a * 2);
-console.log(a / 2);
+// console.log(a + 2);
+// console.log(a * 2);
+// console.log(a / 2);
 
-const b = [1,2,3,4,5];
-console.log(b);
-console.log(b[2]);
-b.push("101");
-console.log(b);
+// const b = [1,2,3,4,5];
+// console.log(b);
+// console.log(b[2]);
+// b.push("101");
+// console.log(b);
 
-const c = {
-    name: "kosw",
-    points: 10,
-    fat: true,
-};
+// const c = {
+//     name: "kosw",
+//     points: 10,
+//     fat: true,
+// };
 
-console.log(c);
-console.log(c.name);
+// console.log(c);
+// console.log(c.name);
 
-function d() {
-    console.log("hello");
-}
-d();
+// function d() {
+//     console.log("hello");
+// }
+// d();
 
-const age = 10;
-function e(cnt) {
-    return cnt + 2;
-}
-const krage = e(20);
-console.log(krage);
+// const age = 10;
+// function e(cnt) {
+//     return cnt + 2;
+// }
+// const krage = e(20);
+// console.log(krage);
 
-const age2 = prompt("how old?");
-console.log(typeof "15", typeof parseInt("15"));
+// const age2 = prompt("how old?");
+// console.log(typeof "15", typeof parseInt("15"));
+
+const title = document.querySelector(".hello h1");
+
+
+title.innerText = "Hello";
+console.log(title);
